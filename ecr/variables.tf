872288cf465862846e9f80dbf89ceebc26b6c928
James Jeffrey <james.jeffrey@engineering.digital.dwp.gov.uk>
2021-09-02 15:17:13 +0100

    Various path and variable fixes

--- ecr/variables.tf
+++ ecr/variables.tf
@@ -13,26 +13,3 @@ variable "region" {
   type    = string
   default = "eu-west-2"
 }
-
-variable "github_organization" {
-  type        = string
-  description = "GitHub Organisation to create repos in"
-  default     = "dwp"
-}
-
-variable "github_token" {
-  type        = string
-  description = "GitHub personal access token for managing repos and committing code"
-}
-
-variable "github_email" {
-  type        = string
-  description = "GitHub Email Address for committing code"
-}
-
-variable "github_username" {
-  type        = string
-  description = "GitHub Username for committing code"
-}
-
-
