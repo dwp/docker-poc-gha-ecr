bc9e25d4652ae4362f54ed734db5b0d20478fc45
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-09-02 11:58:26 +0000

    Initial commit, adding githooks submodule

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# dataworks-repo-template-docker
+# docker-poc-gha-ecr
 
-## Description
+## A proof of concept repository for building a docker image in GHA and pushing to ECR with secure STS credentials
 
 This repo contains Makefile, and Dockerfile to fit the standard pattern.
 This repo is a base to create new Docker image repos, adding the githooks submodule, making the repo ready for use.
--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "docker-poc-gha-ecr" {
+  name = "docker-poc-gha-ecr"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/docker-poc-gha-ecr" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "docker-poc-gha-ecr" {
+  repository = aws_ecr_repository.docker-poc-gha-ecr.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.docker-poc-gha-ecr.repository_url
 }
