ac31c8f7b91848dbcbd9a90dadca4475bde6c097
James Jeffrey <james.jeffrey@engineering.digital.dwp.gov.uk>
2021-09-02 15:17:21 +0100

    Various path and variable fixes

---
+++ gha_sts_secrets/variables.tf
@@ -0,0 +1,38 @@
+variable "costcode" {
+  type    = string
+  default = ""
+}
+
+variable "assume_role" {
+  type        = string
+  default     = "ci"
+  description = "IAM role assumed by Concourse when running Terraform"
+}
+
+variable "region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "github_organization" {
+  type        = string
+  description = "GitHub Organisation to create repos in"
+  default     = "dwp"
+}
+
+variable "github_token" {
+  type        = string
+  description = "GitHub personal access token for managing repos and committing code"
+}
+
+variable "github_email" {
+  type        = string
+  description = "GitHub Email Address for committing code"
+}
+
+variable "github_username" {
+  type        = string
+  description = "GitHub Username for committing code"
+}
+
+
