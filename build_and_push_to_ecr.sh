f31f2ef8a644fe6924a461e9b960daa8ff772ded
James Jeffrey <james.jeffrey@engineering.digital.dwp.gov.uk>
2021-09-03 21:20:57 +0100

    Attempt to fix failing workflow

--- build_and_push_to_ecr.sh
+++ build_and_push_to_ecr.sh
@@ -16,7 +16,7 @@ docker login --username AWS --password-stdin "$DEV_REG_URL"
 
 for TAG in "latest" "$SEM_VER" ; do 
   docker tag "$IMAGE_NAME" "$DEV_REG_URL/$IMAGE_NAME:$TAG"
-  docker push "$DEV_REG_URL:$TAG" | sed "s/$DEV_AWS_ACCOUNT/XXXXXXXX/"
+  docker push "$DEV_REG_URL/$IMAGE_NAME:$TAG" | sed "s/$DEV_AWS_ACCOUNT/XXXXXXXX/"
 done
 
 # Live Second
@@ -27,7 +27,7 @@ docker login --username AWS --password-stdin "$REG_URL"
 
 for TAG in "latest" "$SEM_VER" ; do
   docker tag "$IMAGE_NAME" "$REG_URL/$IMAGE_NAME:$TAG"
-  docker push "$REG_URL:$TAG" | sed "s/$AWS_ACCOUNT/XXXXXXXX/"
+  docker push "$REG_URL/$IMAGE_NAME:$TAG" | sed "s/$AWS_ACCOUNT/XXXXXXXX/"
 done
 
 # Complete
