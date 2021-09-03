830bf9754fa0f17dedb12324b89f8acb58ff34e7
James Jeffrey <james.jeffrey@engineering.digital.dwp.gov.uk>
2021-09-03 20:56:17 +0100

    Attempt to fix failing workflow

--- build_and_push_to_ecr.sh
+++ build_and_push_to_ecr.sh
@@ -6,24 +6,28 @@ set -euo pipefail
 cd build
 docker build -t "$IMAGE_NAME" .
 
-# Setup AWS credentials  and URLs etc
-export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
-export DEV_AWS_ACCESS_KEY_ID DEV_AWS_SECRET_ACCESS_KEY DEV_AWS_SESSION_TOKEN
-export AWS_REGION AWS_ACCOUNT DEV_AWS_ACCOUNT
-
-REG_URL="${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com"
-DEV_REG_URL="${DEV_AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com"
-
-# Login to ECR
+# Dev First
+DEV_REG_URL="${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+AWS_ACCESS_KEY_ID="$DEV_AWS_ACCESS_KEY_ID" \
+AWS_SECRET_ACCESS_KEY="$DEV_AWS_SECRET_ACCESS_KEY" \
+AWS_SESSION_TOKEN="$DEV_AWS_SESSION_TOKEN" \
 aws ecr get-login-password --region "$AWS_REGION" | \
-  docker login --username AWS --password-stdin "$REG_URL"
+docker login --username AWS --password-stdin "$DEV_REG_URL"
 
-# Tag and push
 for TAG in "latest" "$SEM_VER" ; do 
-  for URL in "$DEV_REG_URL" "$REG_URL" ; do
-    docker tag "$IMAGE_NAME" "$URL:$TAG"
-    docker push "$URL:$TAG" | sed "s/$ACCOUNT/XXXXXXXX/"
-  done
+  docker tag "$IMAGE_NAME" "$URL:$TAG"
+  docker push "$URL:$TAG" | sed "s/$ACCOUNT/XXXXXXXX/"
+done
+
+# Live Second
+REG_URL="${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+aws ecr get-login-password --region "$AWS_REGION" | \
+docker login --username AWS --password-stdin "$REG_URL"
+
+for TAG in "latest" "$SEM_VER" ; do
+  docker tag "$IMAGE_NAME" "$URL:$TAG"
+  docker push "$URL:$TAG" | sed "s/$ACCOUNT/XXXXXXXX/"
 done
 
 # Complete
