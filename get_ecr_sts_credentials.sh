072f7bc2d3b64952b4fa3d69eb2deda693d26a77
James Jeffrey <james.jeffrey@engineering.digital.dwp.gov.uk>
2021-09-06 14:43:18 +0100

    Re-order creation of credentials

--- get_ecr_sts_credentials.sh
+++ get_ecr_sts_credentials.sh
@@ -7,15 +7,6 @@ set +x
 
 DURATION="${ASSUME_DURATION:=1800}"
 
-# Generate Live Management Credentials
-MGMT_ACCOUNT="$(jq -r .management < accounts.json)"
-ROLE_ARN="arn:aws:iam::$MGMT_ACCOUNT:role/gha_ecr"
-AWS_STS="$(aws sts assume-role --role-arn "$ROLE_ARN" --role-session-name "ci-$(date +%m%d%y%H%M%S)" --duration-seconds ${DURATION})"
-
-SECRET_ACCESS_KEY="$(echo "$AWS_STS" | jq .Credentials.SecretAccessKey -r)"
-ACCESS_KEY_ID="$(echo "$AWS_STS" | jq .Credentials.AccessKeyId -r)"
-SESSION_TOKEN="$(echo "$AWS_STS" | jq .Credentials.SessionToken -r)"
-
 # Generate Dev Management Credentials
 DEV_MGMT_ACCOUNT="$(jq -r '."management-dev"' < accounts.json)"
 ROLE_ARN="arn:aws:iam::$DEV_MGMT_ACCOUNT:role/gha_ecr"
@@ -24,6 +15,15 @@ AWS_STS="$(aws sts assume-role --role-arn "$ROLE_ARN" --role-session-name "dev-c
 DEV_SECRET_ACCESS_KEY="$(echo "$AWS_STS" | jq .Credentials.SecretAccessKey -r)"
 DEV_ACCESS_KEY_ID="$(echo "$AWS_STS" | jq .Credentials.AccessKeyId -r)"
 DEV_SESSION_TOKEN="$(echo "$AWS_STS" | jq .Credentials.SessionToken -r)"
+
+## Generate Live Management Credentials
+MGMT_ACCOUNT="$(jq -r .management < accounts.json)"
+ROLE_ARN="arn:aws:iam::$MGMT_ACCOUNT:role/gha_ecr"
+AWS_STS="$(aws sts assume-role --role-arn "$ROLE_ARN" --role-session-name "ci-$(date +%m%d%y%H%M%S)" --duration-seconds ${DURATION})"
+
+SECRET_ACCESS_KEY="$(echo "$AWS_STS" | jq .Credentials.SecretAccessKey -r)"
+ACCESS_KEY_ID="$(echo "$AWS_STS" | jq .Credentials.AccessKeyId -r)"
+SESSION_TOKEN="$(echo "$AWS_STS" | jq .Credentials.SessionToken -r)"
 
 # Write Credentials to Output
 cat > gha_ecr_sts_creds <<EOT
